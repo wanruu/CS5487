ae6b4a5077c9b5d11614b4a5355b14d1f2638d4e
Wendy <1155107790@link.cuhk.edu.hk>
2022-11-30 10:43:10 +0800

    add get_device

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 
 def accuracy(predict, actual) -> float:
@@ -5,3 +6,8 @@ def accuracy(predict, actual) -> float:
     actual = np.array(actual).flatten()
     result = predict == actual
     return np.sum(result) / predict.shape[0]  # [0,1]
+
+
+def get_device():
+    return "cuda:0" if torch.cuda.is_available() else "cpu"
+
