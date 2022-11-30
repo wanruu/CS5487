8d50d34d5cc427fea07e98c44e5fcf08dbeb4315
Wendy <1155107790@link.cuhk.edu.hk>
2022-11-30 13:32:50 +0800

    improve cnn

--- cnn_models.py
+++ cnn_models.py
@@ -21,26 +21,45 @@ class EarlyStopping:
         if self.cnt >= self.patience:
             self.flag = True
 
-
-class CNN_1(nn.Module):
+class CNN_2(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.name = "cnn_1"
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32,
-                      kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(1600, 10)
+
+    def forward(self, input):
+        out = self.conv1(input)
+        out = self.conv2(out)
+        out = self.flatten(out)
+        out = self.fc(out)
+        return out
+
+
+class CNN_3(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64,
-                      kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=64,  kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64,
-                      kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2)
         )
@@ -56,33 +75,7 @@ class CNN_1(nn.Module):
         return out
 
 
-class CNN_2(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "cnn_2"
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32,
-                      kernel_size=5, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64,
-                      kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2)
-        )
-        self.flatten = nn.Flatten()
-        self.fc = nn.Linear(2304, 10)
-
-    def forward(self, input):
-        out = self.conv1(input)
-        out = self.conv2(out)
-        out = self.flatten(out)
-        out = self.fc(out)
-        return out
-
 if __name__ == "__main__":
     from torchsummary import summary 
-    model = CNN_1()
+    model = CNN_2()
     summary(model, (1, 28, 28))
