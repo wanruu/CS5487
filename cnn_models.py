07a74d67ae0dc27bff1ca1fb9c1ea398d54cfcc8
Wendy <1155107790@link.cuhk.edu.hk>
2022-11-30 12:50:55 +0800

    fix bug of CNN_2

--- cnn_models.py
+++ cnn_models.py
@@ -59,31 +59,33 @@ class CNN_2(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.name = "cnn_2"
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.mp = nn.MaxPool2d(2)
+
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32,
-                      kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=10,
+                      kernel_size=5, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64,
-                      kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64,
+            nn.Conv2d(in_channels=10, out_channels=20,
                       kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
         )
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(576, 10)
+        self.fc = nn.Linear(720, 10)
 
     def forward(self, input):
         out = self.conv1(input)
         out = self.conv2(out)
-        out = self.conv3(out)
         out = self.flatten(out)
         out = self.fc(out)
         return out
+
+if __name__ == "__main__":
+    from torchsummary import summary 
+    model = CNN_2()
+    summary(model, (1, 28, 28))
